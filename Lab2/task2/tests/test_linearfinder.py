6ae4d0f66bb63481e8782350ffb86bb5f327f29a
vzhuhfaka <ostanin20.06.a@gmail.com>
2024-10-13 16:01:40 +0300

    Написан юнит тест для linearfinder.py

---
+++ Lab2/task2/tests/test_linearfinder.py
@@ -0,0 +1,17 @@
+import unittest
+from Lab2.task2.src.linearfinder import linearfinder
+
+def txt_to_list(file_txt):
+    res = []
+    with open(file_txt, 'r') as f:
+        for i in f.readline():
+            if i in '0123456789':
+                res.append(i)
+    return res
+
+
+class TestLinearFinder(unittest.TestCase):
+
+    def test_linearfinder(self):
+        linearfinder('test_input.txt', 'test_output.txt')
+        self.assertEqual(txt_to_list('test_output.txt'), ['3', '9'])
