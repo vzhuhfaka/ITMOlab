60d801ee8968cd052cd3c579e6c3c64fb95840b7
vzhuhfaka <ostanin20.06.a@gmail.com>
2024-10-13 15:46:54 +0300

    linearfinder.py весь код помещён в функцию linearfinder()

--- Lab2/task1/src/insertionsort.py
+++ Lab2/task1/src/insertionsort.py
@@ -5,29 +5,30 @@ def checker(ar):
     return True
 
 
-file_input = open('input.txt', 'r')
-file_output = open('output.txt', 'w+')
-n = int(file_input.readline())
-s = [int(el) for el in file_input.readline().split()]
+def insertionsort(file_in, file_out):
+    file_input = open(file_in, 'r')
+    file_output = open(file_out, 'w+')
+    n = int(file_input.readline())
+    s = [int(el) for el in file_input.readline().split()]
 
-if not (1 <= n <= 10**3):
-    file_output.write('Wrong first number')
-    exit('Wrong first number')
+    if not (1 <= n <= 10**3):
+        file_output.write('Wrong first number')
+        exit('Wrong first number')
 
-if not checker(s):
-    file_output.write('One of the numbers goes out of bounds')
-    exit('One of the numbers goes out of bounds')
+    if not checker(s):
+        file_output.write('One of the numbers goes out of bounds')
+        exit('One of the numbers goes out of bounds')
 
-for i in range(1, len(s)):
-    value = s[i]
-    j = i - 1
-    while value < s[j] and j >= 0:
-        s[j+1] = s[j]
-        j -= 1
-    s[j+1] = value
+    for i in range(1, len(s)):
+        value = s[i]
+        j = i - 1
+        while value < s[j] and j >= 0:
+            s[j+1] = s[j]
+            j -= 1
+        s[j+1] = value
 
-for elem in s:
-    file_output.write(f'{elem} ')
+    for elem in s:
+        file_output.write(f'{elem} ')
 
-file_input.close()
-file_output.close()
+    file_input.close()
+    file_output.close()
