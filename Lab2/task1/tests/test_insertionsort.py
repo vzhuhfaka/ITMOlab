3dc41a80292ecb42475e7897c3b203fba44ca07f
vzhuhfaka <ostanin20.06.a@gmail.com>
2024-10-13 15:57:17 +0300

    Написан юниттест для insertionsort.py

---
+++ Lab2/task1/tests/test_insertionsort.py
@@ -0,0 +1,16 @@
+import unittest
+from Lab2.task1.src.insertionsort import insertionsort
+
+def txt_to_list(file_txt):
+    res = []
+    with open(file_txt, 'r') as f:
+        for i in f.readline().split():
+            res.append(int(i))
+    return res
+
+
+class TestInsertionSort(unittest.TestCase):
+
+    def test_insertionsort(self):
+        insertionsort('test_input.txt', 'test_output.txt')
+        self.assertEqual(txt_to_list('test_output.txt'), [26, 31, 41, 41, 58, 59])
