85fa257e93ccadefe709ac2a3433bc3a1b4e5d52
vzhuhfaka <ostanin20.06.a@gmail.com>
2024-10-14 15:17:41 +0300

    insertionsort добавлен подсчёт затраченного времени и памяти на выполнение

--- Lab2/task1/tests/test_insertionsort.py
+++ Lab2/task1/tests/test_insertionsort.py
@@ -12,5 +12,5 @@ def txt_to_list(file_txt):
 class TestInsertionSort(unittest.TestCase):
 
     def test_insertionsort(self):
-        insertionsort('test_input.txt', 'test_output.txt')
+        insertionsort('test_input.txt', 'test_output.txt', 'test_info.txt')
         self.assertEqual(txt_to_list('test_output.txt'), [26, 31, 41, 41, 58, 59])
