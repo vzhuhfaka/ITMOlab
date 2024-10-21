7976975f489a0b094ee99c2fc46957792280bbf8
vzhuhfaka <ostanin20.06.a@gmail.com>
2024-10-21 18:56:38 +0300

    refactor

--- lab1/task3/tests/test_binsum.py
+++ lab1/task3/tests/test_binsum.py
@@ -6,5 +6,5 @@ from utils import txt_to_str
 class TestBinSum(unittest.TestCase):
 
     def test_binsum(self):
-        main_binsum('Lab1/task3/tests/test_input.txt', 'Lab1/task3/tests/test_output.txt', 'Lab1/task3/tests/test_time.txt')
-        self.assertEqual(txt_to_str('Lab1/task3/tests/test_output.txt'), '11101')
+        main_binsum('Lab1/task3/txtf/test_input.txt', 'Lab1/task3/txtf/test_output.txt', 'Lab1/task3/tests/test_time.txt')
+        self.assertEqual(txt_to_str('Lab1/task3/txtf/test_output.txt'), '11101')
